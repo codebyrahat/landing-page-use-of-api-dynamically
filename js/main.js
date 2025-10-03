a606e55525a156252f1c84cafca0bb1759f2f40c
Rahat.dev <rahatalam.webdev@gmail.com>
2025-10-03 17:51:54 +0600

    dynamic title container completed

--- js/main.js
+++ js/main.js
@@ -4,36 +4,33 @@
 
 // error-show container id---global declaration
 const errorShow = document.getElementById("error-msg");
-const showAll = document.getElementById('show-all');
+const showAll = document.getElementById("show-all");
 // getting card container
 const cardContainer = document.getElementById("dynamic-card-container");
-
 
 // displayData function
 const displayData = (posts, isShowAll) => {
   cardContainer.textContent = "";
-  showAll.classList.add('hidden');
+  showAll.classList.add("hidden");
   // updating error msg, if search context is unavailable
   if (posts.length < 1) {
-    errorShow.classList.remove('text-green-400');
-    errorShow.classList.add('text-red-600')
+    errorShow.classList.remove("text-green-400");
+    errorShow.classList.add("text-red-600");
     errorShow.textContent = "This context is not available";
   }
   // customizing error show container if context is available
   else if (posts.length > 0 && posts.length < 4) {
-    errorShow.classList.remove('text-red-600');
-    errorShow.classList.add('text-green-400');
+    errorShow.classList.remove("text-red-600");
+    errorShow.classList.add("text-green-400");
     errorShow.textContent = "Search Successful";
-  }
-  else {
+  } else {
     errorShow.textContent = " ";
   }
   //  slice to enable show all button
   if (posts.length > 3 && !isShowAll) {
     posts = posts.slice(0, 3);
-    showAll.classList.remove('hidden');
+    showAll.classList.remove("hidden");
   }
-
 
   //  for each loop
   posts.forEach((post) => {
@@ -43,17 +40,20 @@ const displayData = (posts, isShowAll) => {
     //  card inner html structure , made in html then pasted here
     card.innerHTML = `
   <div class="w-1/3">
-   <img class="w-full h-full rounded-2xl object-cover object-center" src="${post.image
-      }" alt="" />
+   <img class="w-full h-full rounded-2xl object-cover object-center" src="${
+     post.image
+   }" alt="" />
 </div>
 <!-- content div -->
-<div class="flex flex-col lg:gap-10 gap-2">
+<div class="flex flex-col justify-between flex-1 lg:gap-10 gap-2">
   <!-- #music div -->
   <div class="flex items-center gap-2 lg:gap-5">
     <!-- category -->
     <p class="font-bold">Category#${post.category}</p>
     <!-- author -->
-    <p class="font-bold">Author: ${post.author?.name ? post.author.name : "No author"}</p>
+    <p class="font-bold">Author: ${
+      post.author?.name ? post.author.name : "No author"
+    }</p>
   </div>
   <!-- title div -->
   <div>
@@ -66,7 +66,7 @@ const displayData = (posts, isShowAll) => {
     <p>${post.description}</p>
   </div>
   <!-- Icons and button div -->
-  <div class="flex items-center justify-between">
+  <div class="w-full flex items-center justify-between">
     <!-- icon div container -->
     <div class="flex gap-2 lg:gap-12 items-center">
       <!-- comment_count icon  -->
@@ -91,7 +91,9 @@ const displayData = (posts, isShowAll) => {
     
     <!-- button div start-->
     <div>
-      <button><img src="svg/mail.svg" alt=""></button>
+      <button onclick="countTitle('${post.title}' ,${
+      post.view_count
+    })" class="btn btn-primary rounded-full p-1 bg-black"><img src="svg/mail.svg" alt=""></button>
     </div>
 
   </div>
@@ -100,9 +102,8 @@ const displayData = (posts, isShowAll) => {
 
     // appending child into  card container
     cardContainer.appendChild(card);
-  })
+  });
 };
-
 
 // getData function
 const getData = async (searchText, isShowAll) => {
@@ -112,9 +113,7 @@ const getData = async (searchText, isShowAll) => {
       res = await fetch(
         `https://openapi.programming-hero.com/api/retro-forum/posts?category=`
       );
-
-    }
-    else {
+    } else {
       res = await fetch(
         `https://openapi.programming-hero.com/api/retro-forum/posts?category=${searchText}`
       );
@@ -131,21 +130,45 @@ const getData = async (searchText, isShowAll) => {
   }
 };
 
-
 // btnClicked function
 const btnClicked = (isShowAll) => {
   const searchField = document.getElementById("search-field");
   const searchText = searchField.value;
   getData(searchText, isShowAll);
   setTimeout(() => {
-    searchField.value = '';
+    searchField.value = "";
   }, 500);
 };
 
 // show all function
 const doShowAll = () => {
-
   btnClicked(true);
-  showAll.classList.add('hidden');
+  showAll.classList.add("hidden");
+};
 
-}
+// getTitle function
+const counter = document.getElementById("counter");
+const titleContainer = document.getElementById("dynamic-title-container");
+let count = 0;
+let addedTitle = new Set();
+
+const countTitle = (title, viewCount) => {
+  if (addedTitle.has(title)) {
+    return;
+  } else {
+    count++;
+    counter.innerText = count;
+    addedTitle.add(title);
+    const newTitleDiv = document.createElement("div");
+    newTitleDiv.classList = `rounded-2xl lg:p-4 p-2 bg-white`;
+    newTitleDiv.innerHTML = `
+    <div class="flex gap-5 items-center font-bold">
+    <p>${title}</p>
+    <div class="flex gap-2 items-center">
+    <img src="svg/eye.svg" alt="svg icon of view_count">
+    <span>${viewCount}</span>
+    </div>
+    </div>`;
+    titleContainer.appendChild(newTitleDiv);
+  }
+};
